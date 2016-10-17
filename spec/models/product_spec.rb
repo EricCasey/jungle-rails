5e3889aa2358ee98baf82df1af2670c574c86ac0
EricCasey <ericcasey509@gmail.com>
2016-10-17 16:22:39 +0000

    adding tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  describe 'Validation' do
+
+    before(:each) do
+      @category = Category.new
+      @category['id'] = 100
+      @product = Product.new
+      @product['category_id'] = @category.id
+      @product['name'] = 'test product 123 OMG'
+      @product['quantity'] = 100
+      @product['price_cents'] = 101.to_i
+    end
+
+    it 'should validate precence of :price' do
+        expect(@product).to validate_presence_of :price_cents
+        puts @product.errors.size
+        puts @product.errors.full_messages
+    end
+    it 'should validate precence of :name' do
+        expect(@product).to validate_presence_of :name
+        puts @product.errors.size
+        puts @product.errors.full_messages
+    end
+    it 'should validate precence of :category_id' do
+        expect(@product).to validate_presence_of :category_id
+        puts @product.errors.size
+        puts @product.errors.full_messages
+    end
+    it 'should validate precence of :quantity' do
+        expect(@product).to validate_presence_of :quantity
+        puts @product.errors.size
+        puts @product.errors.full_messages
+    end
+  end
+
+    it 'new product should have no name' do
+      @product = Product.new()
+      expect(@product.name).to be_nil
+      puts @product.errors.size
+      puts @product.errors.full_messages
+    end
+end
